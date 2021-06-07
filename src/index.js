0ea3d433b9df92d5eeaf7e4a4ade17a177f25b07
Diego <diegodsha@hotmail.com>
2021-06-07 10:44:06 -0500

    Work on styles and layout

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import './style.scss';
 
 // const cardBkg = document.getElementById('cardBack');
 // cardBkg.src = cardBack;
+
+const searchBtn = document.getElementById('search');
+const location = document.getElementById('location');
+const cityName = document.querySelector('.city-name');
+
+const searchWeather = (e) => {
+  e.preventDefault();
+  const cityCountry = location.value.trim().split(/\s+/);
+  console.log(cityCountry);
+  cityName.innerText = cityCountry[0];
+};
+
+searchBtn.addEventListener('click', searchWeather);
